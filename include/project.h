f8b4fb474f8c32355be59e0fd8e97cfdbc516672
Matthew <buchkowski@protonmail.com>
2025-12-07 15:00:29 -0500

    added the initialization of i2c. Now the esp32 will create a new i2c instance/bus, mux the appropriate pins and clock to it, and finally configure it with a basic device configuration for the i2c bus/port.
    Also briefly tested the init functions for BME and seems we are properly handling errors.

---
+++ include/project.h
@@ -0,0 +1,53 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "stdio.h"
+#include "string.h"
+#include "esp_log.h"
+#include "bme68x.h"
+#include "driver/i2c.h"
+#include "driver/i2c_master.h"
+
+#define LED GPIO_NUM_2
+#define DELAY 800
+#define I2C_FREQ_HZ         400000     // 400kHz
+#define I2C_PORT            I2C_NUM_0
+
+#define DEVICE_ADDR         0x3C       // example device address
+
+
+
+gpio_config_t led_config = {
+    .intr_type = GPIO_INTR_DISABLE,
+    .mode = GPIO_MODE_OUTPUT,
+    .pin_bit_mask = (1U << LED),
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .pull_up_en = GPIO_PULLUP_DISABLE
+};
+
+i2c_master_bus_config_t i2c_bus_config = {
+    .clk_source = I2C_CLK_SRC_DEFAULT,
+    .scl_io_num = GPIO_NUM_22,
+    .sda_io_num = GPIO_NUM_21,
+    .i2c_port = I2C_PORT,
+    .flags.enable_internal_pullup = true,
+    .glitch_ignore_cnt = 7,
+    .intr_priority = 0
+};
+
+i2c_device_config_t i2c_dev_config = {
+    .dev_addr_length = 1,
+    .device_address = 0x20,
+    .scl_speed_hz = I2C_FREQ_HZ,
+    .scl_wait_us = 0,
+    .flags.disable_ack_check = false
+};
+
+i2c_master_bus_handle_t i2c_bus_handle;
+i2c_master_dev_handle_t i2c_dev_handle;
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,28 +1,81 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "stdio.h"
-#include "string.h"
-#include "esp_log.h"
-
-#define LED GPIO_NUM_2
-#define DELAY 200
+#include "project.h"
 
 static const char* tag = "BME680 Project";
+struct bme68x_dev bme;
+struct bme68x_conf bme_conf;
+struct bme68x_data bme_data;
 
-gpio_config_t led_config = {
-    .intr_type = GPIO_INTR_DISABLE,
-    .mode = GPIO_MODE_OUTPUT,
-    .pin_bit_mask = (1U << LED),
-    .pull_down_en = GPIO_PULLDOWN_DISABLE,
-    .pull_up_en = GPIO_PULLUP_DISABLE
-};
+
+
+
+void bme68x_check_rslt(const char api_name[], int8_t rslt)
+{
+    switch (rslt)
+    {
+        case BME68X_OK:
+
+            /* Do nothing */
+            break;
+        case BME68X_E_NULL_PTR:
+            printf("API name [%s]  Error [%d] : Null pointer\r\n", api_name, rslt);
+            break;
+        case BME68X_E_COM_FAIL:
+            printf("API name [%s]  Error [%d] : Communication failure\r\n", api_name, rslt);
+            break;
+        case BME68X_E_INVALID_LENGTH:
+            printf("API name [%s]  Error [%d] : Incorrect length parameter\r\n", api_name, rslt);
+            break;
+        case BME68X_E_DEV_NOT_FOUND:
+            printf("API name [%s]  Error [%d] : Device not found\r\n", api_name, rslt);
+            break;
+        case BME68X_E_SELF_TEST:
+            printf("API name [%s]  Error [%d] : Self test error\r\n", api_name, rslt);
+            break;
+        case BME68X_W_NO_NEW_DATA:
+            printf("API name [%s]  Warning [%d] : No new data found\r\n", api_name, rslt);
+            break;
+        default:
+            printf("API name [%s]  Error [%d] : Unknown error code\r\n", api_name, rslt);
+            break;
+    }
+}
+
+/*!
+ * I2C read function map to ESP32 platform
+ */
+esp_err_t bme68x_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+    uint8_t device_addr = *(uint8_t*)intf_ptr;
+
+    (void)intf_ptr;
+
+    return ESP_OK;
+}
+
+/*!
+ * I2C write function map to ESP32 platform
+ */
+esp_err_t bme68x_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+    uint8_t device_addr = *(uint8_t*)intf_ptr;
+
+    (void)intf_ptr;
+    return ESP_OK;
+}
 
 
 void setup(void)
 {
     ESP_LOGI(tag, "Setting up project");
-    gpio_config(&led_config);
+    ESP_ERROR_CHECK(gpio_config(&led_config));
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_config, &i2c_bus_handle));
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &i2c_dev_config, &i2c_dev_handle));
+
+    int8_t rslt = bme68x_init(&bme);
+    bme68x_check_rslt("bme68x_init", rslt);
+    rslt = bme68x_get_conf(&bme_conf, &bme);
+    bme68x_check_rslt("bme68x_get_conf", rslt);
+
 }
 
 void app_main() 
